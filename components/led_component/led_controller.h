23b357df1187d7f4a001d10fce7b7f55e5ec26b1
Pablo Gonzalez <pablovglez@gmail.com>
2026-07-15 13:53:08 +0200

    wip: add led responsive features

---
+++ components/led_component/led_controller.h
@@ -0,0 +1,20 @@
+#ifndef LED_CONTROLLER_H
+#define LED_CONTROLLER_H
+#define MAX_LED_COUNT 1
+
+typedef struct {
+    int led_pin;
+    int timer_down;
+    int timer_up;
+    int occurrences; // Number of times the timer has been triggered
+} led_timer_message_t;
+
+void init_led();
+void led_on(int led_pin);
+void led_off(int led_pin);
+void stop_led_processing_task();
+
+QueueHandle_t get_led_timer_queue();
+void led_processing_task(void *pvParameters);
+
+#endif // LED_CONTROLLER_H
